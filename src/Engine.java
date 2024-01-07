38e2653f0a5463ecd5259013e344c95baa6b9d6c
maxim158 <m-ermichin@mail.ru>
2024-01-07 19:13:27 +0300

    Reworked Version 2.1

--- src/Engine.java
+++ src/Engine.java
@@ -1,5 +1,3 @@
-
-
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
@@ -11,40 +9,42 @@ import java.util.Random;
 
 import static java.lang.Math.abs;
 
-public class Engine {
-    int n, m, target, move, score, curI, curJ, prevI, prevJ, amove;
+public class Engine implements ActionListener {
+    int n, m, target, move, score, amove, previous, current;
+    int MaxNumberOnTheField = 0;
+    boolean load;
     JFrame frame = new JFrame("More or less, less is more");
     JButton[][] b;
     JLabel l = new JLabel();
     JLabel mo = new JLabel();
-    int[][] ctr;
-    String temp;
     Random r = new Random();
-    Engine(int n, int m, int move, int target,int score, int amove){
+
+    Engine(int n, int m, int move, int target, int score, int amove, boolean load, String[][] field) {
         this.n = n;
         this.m = m;
         this.target = target;
         this.move = move;
         this.score = score;
         this.amove = amove;
+        this.load = load;
+        this.previous = 1;
+        this.current = 1;
 
-        frame.setLocation(100,200);
-        frame.setSize(400,500);
+        frame.setLocation(100, 200);
+        frame.setSize(400, 500);
         frame.setLocationRelativeTo(null);
         frame.getContentPane().setBackground(Color.white);
         l.setText("Score: " + score + ", Target value: " + target);
-        frame.add(l,BorderLayout.NORTH);
+        frame.add(l, BorderLayout.NORTH);
         mo.setText("The number of moves: " + move);
-        frame.add(mo,BorderLayout.SOUTH);
+        frame.add(mo, BorderLayout.SOUTH);
 
         JMenuBar menuBar = new JMenuBar();
         JMenu menu = new JMenu("Options");
         JMenuItem rst = new JMenuItem("Restart");
         JMenuItem save = new JMenuItem("Save");
-        JMenuItem load = new JMenuItem("Load");
         menu.add(rst);
         menu.add(save);
-        menu.add(load);
         menuBar.add(menu);
         frame.setJMenuBar(menuBar);
         rst.addActionListener(new ActionListener() {
@@ -69,14 +69,12 @@ public class Engine {
                     File file = schooser.getSelectedFile();
                     try {
                         BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-                        writer.write("Score: " + score + " Moves: " + move + "\n");
-                        for (int i = 0; i<n;i++) {
+                        writer.write(n + "," + m + "," + target + "," + amove + "\n");
+                        for (int i = 0; i < n; i++) {
                             for (int j = 0; j < m; j++) {
-                                if (j % m == 0){
-                                    writer.write("\n");
-                                }
-                                writer.write(b[i][j].getText() + " ");
+                                writer.write(b[i][j].getText() + ",");
                             }
+                            writer.write('\n');
                         }
                         writer.close();
                     } catch (Exception ex) {
@@ -87,111 +85,70 @@ public class Engine {
             }
         });
 
-        load.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int[][] field = new int[n][m];
-                JFileChooser lchooser = new JFileChooser();
-                FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files", "txt");
-                lchooser.setFileFilter(filter);
-                lchooser.setCurrentDirectory(new File("."));
-                lchooser.setDialogTitle("Load");
-                int res = lchooser.showSaveDialog(null);
-                if (res == JFileChooser.APPROVE_OPTION) {
-                    File file = lchooser.getSelectedFile();
-                    String filePath = file.getAbsolutePath();
-                    try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-                        String line;
-                        int row = 0;
-                        while ((line = br.readLine()) != null && row < m) {
-                            String[] values = line.trim().split("\\s+");
-                            for (int col = 0; col < n; col++) {
-                                if (!values[col].isEmpty()) {
-                                    field[row][col] = Integer.parseInt(values[col]);
-                                }
-                            }
-                            row++;
-                        }
-                    } catch (IOException | NumberFormatException t) {
-                        t.printStackTrace();
-                    }
-                    for (int i = 0; i < n; i++) {
-                        for (int j = 0; j < m; j++) {
-                            b[i][j].setText(field[i][j] + "");
-                            b[i][j].setBackground(new Color(0xFFFFFF));
-                        }
-                    }
-                }
-            }
-        });
-
         JPanel panel = new JPanel();
-        GridLayout gl = new GridLayout(n,m);
+        GridLayout gl = new GridLayout(n, m);
         panel.setLayout(gl);
 
-        ctr = new int[n][m];
+        int randomNumber = 1;
         b = new JButton[n][m];
-        for (int i = 0; i<n;i++){
-            for (int j=0; j<m;j++){
-                ctr[i][j] = r.nextInt(1,10);
-                b[i][j] = new JButton(ctr[i][j]+"");
-                b[i][j].setBackground(new Color(0xFFFFFF));
-                //suggestions
-                if (move == move){
-                    if (target >= 9 && Integer.parseInt(b[i][j].getText())==Math.max(1,9)){
-                        b[i][j].setBackground(new Color(0x6EC510));
-                    }
-                    if (target == Integer.parseInt(b[i][j].getText())){
-                        b[i][j].setBackground(new Color(0x6EC510));
-                    }
-                }
-                Coloring(i,j,n,m);
-                calc(i,j);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                randomNumber = r.nextInt(9) + 1;
+
+                if (load) b[i][j] = new JButton(field[i][j]);
+                else b[i][j] = new JButton(randomNumber + "");
+
+                if (target == Integer.parseInt(b[i][j].getText())) b[i][j].setBackground(new Color(0x6BFD07));
+                else b[i][j].setBackground(new Color(0xFFFFFF));
+                MaxNumberOnTheField = Math.max(MaxNumberOnTheField, Integer.parseInt(b[i][j].getText()));
+                b[i][j].addActionListener(this);
+                calc(i, j);
                 panel.add(b[i][j]);
             }
         }
-        frame.add(panel,BorderLayout.CENTER);
+        if (target > 9) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (MaxNumberOnTheField == Integer.parseInt(b[i][j].getText()))
+                        b[i][j].setBackground(new Color(0x6BFD07));
+                }
+            }
+        }
+        frame.add(panel, BorderLayout.CENTER);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
     }
-    public void Coloring(int iC, int jC, int n, int m){
-        Random r = new Random();
-        b[iC][jC].addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                for (int i = 0; i<n;i++){
-                    for (int j=0; j<m;j++){
-                        //possible nums for choice
-                        if (iC == i || jC == j){ //same column and same raw
-                            b[i][j].setBackground(new Color(0x6BFD07));
-                            b[i][j].setEnabled(true);
-                        }else { //impossible nums for choice
-                            b[i][j].setBackground(new Color(0xF5F5F5));
-                            b[i][j].setEnabled(false);
-                        }
-                        //selected num
-                        if (iC == i && jC == j){
-                            curI = i; //will use for swapping
-                            curJ = j; //will use for swapping
-                            b[i][j].setBackground(new Color(0x78DA64));
-                            b[i][j].setEnabled(false);
-                        }
-                        //omg swapping XD
-                        if (move <= amove - 2){
-                            temp = b[curI][curJ].getText();
-                            System.out.println(temp);
-                            b[curI][curJ].setText(b[prevI][prevJ].getText());
-                            b[prevI][prevJ].setText(temp);
-                            prevI = curI;
-                            prevJ = curJ;
-                        }
-                    }
-                }
-            }
-        });
+
+    public void actionPerformed(ActionEvent e) {
+        JButton o = (JButton) e.getSource();
+        int value = Integer.parseInt(o.getText());
+        Coloring(value);
+        // o.setText(r.nextInt(9) + 1 +"");
+        o.setEnabled(false);
+
     }
-    public void calc(int i, int j){
+
+    public void Coloring(int value) {
+
+        previous = current;
+        current = value;
+
+        for (int row = 0; row < n; row++) {
+            for (int column = 0; column < m; column++) {
+                b[row][column].setBackground(new Color(0xF5F5F5));
+                b[row][column].setEnabled(false);
+
+                if (row % current == 0 && column % previous == 0) {
+                    b[row][column].setBackground(new Color(0x6BFD07));
+                    b[row][column].setEnabled(true);
+                } else if (row % current == 0 || column % previous == 0)
+                    b[row][column].setBackground(new Color(0x78DA64));
+            }
+        }
+    }
+
+    public void calc(int i, int j) {
         b[i][j].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -199,20 +156,20 @@ public class Engine {
                 mo.setText("The number of moves: " + move);
                 int temp = Integer.parseInt(b[i][j].getText());
                 score = score + temp;
-                l.setText("Score: "+ score + ",Target value: " + target);
-                if (move == 0){
+                l.setText("Score: " + score + ",Target value: " + target);
+                if (move == 0) {
                     int rem = abs(target - score);
-                    JOptionPane.showMessageDialog(null, "You lost !!!\nYou ran out of moves\nRemaining score: "+ rem, "GAME OVER!", JOptionPane.WARNING_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "You lost !!!\nYou ran out of moves\nRemaining score: " + rem, "GAME OVER!", JOptionPane.WARNING_MESSAGE);
                     frame.dispose();
                     Menu m = new Menu();
                 }
-                if (score > target){
-                    JOptionPane.showMessageDialog(null, "You lost !!!\nYour score is bigger than the target","GAME OVER!", JOptionPane.WARNING_MESSAGE);
+                if (score > target) {
+                    JOptionPane.showMessageDialog(null, "You lost !!!\nYour score is bigger than the target", "GAME OVER!", JOptionPane.WARNING_MESSAGE);
                     frame.dispose();
                     Menu m = new Menu();
                 }
-                if(score == target){
-                    JOptionPane.showMessageDialog(null, "YOU WIN !!!\nCongratulations","GAME OVER!", JOptionPane.INFORMATION_MESSAGE);
+                if (score == target) {
+                    JOptionPane.showMessageDialog(null, "YOU WIN !!!\nCongratulations", "GAME OVER!", JOptionPane.INFORMATION_MESSAGE);
                     frame.dispose();
                     Menu m = new Menu();
                 }
